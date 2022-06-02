2c2b298a5aae8c5e2370b317ec53d64c733d9942
vishalpraja777 <vishalprajapathi7@gmail.com>
2022-06-02 19:03:15 +0530

    Basic Scenery

--- petrolBunk.c
+++ petrolBunk.c
@@ -5,7 +5,7 @@
 
 static GLfloat theta[] = {0.0,0.0,0.0};
 static GLint axis = 2;
-static GLdouble viewer[]= {10.0, 10.0, 10.0};
+static GLdouble viewer[]= {0.0, 16.0, 30.0};
 
 void display(void)
 {
@@ -15,25 +15,90 @@ void display(void)
 
 	glLoadIdentity();
 	
-	gluLookAt(viewer[0],viewer[1],viewer[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(viewer[0],viewer[1],viewer[2], 0.0, 10.0, 0.0, 0.0, 1.0, 0.0);
 	
+	//platform
 	//glLoadIdentity();
-	glColor3f(0.0,1.0,0.1);
+	glColor3f(0.5,0.5,0.5);
 	glPushMatrix();
 	glTranslatef(0.0,0.0,0.0);
-	glScalef(50.0,1.0,50.0);
- 	glutSolidCube(1.0);
+	glScalef(1.0,0.05,1.0);
+ 	glutSolidCube(30.0);
  	glPopMatrix();
  	
- 	//glLoadIdentity();
- 	glColor3f(1.0,0.0,0.1);
- 	glPushMatrix();
- 	glTranslatef(10.0,0.0,0.0);
- 	//glutSolidCube(5.0);
+ 	//shed
+ 	glColor3f(1.0,0.5,0.0);
+	glPushMatrix();
+	glTranslatef(0.0,17.5,0.0);
+	glScalef(1.0,0.05,1.0);
+ 	glutSolidCube(25.0);
  	glPopMatrix();
+ 	
+ 	//pumpPlatform
+ 	//glLoadIdentity();
+ 	glColor3f(0.3,0.3,0.3);
+ 	glPushMatrix();
+ 	glTranslatef(0.0,1.0,-5.0);
+ 	glScalef(8.0,0.5,2.0);
+ 	glutSolidCube(2.0);
+ 	glPopMatrix();
+ 	
+ 	//glColor3f(1.0,0.0,0.1);
+ 	glPushMatrix();
+ 	glTranslatef(0.0,1.0,5.0);
+ 	glScalef(8.0,0.5,2.0);
+ 	glutSolidCube(2.0);
+ 	glPopMatrix();
+ 	
+ 	//pillars
+ 	GLfloat lx = 5.0;
+ 	GLfloat ly = 8.0;
+ 	GLfloat lz = 5.0;
+ 	
+ 	GLfloat sx = 0.05;
+ 	GLfloat sy = 1.0;
+ 	GLfloat sz = 0.05;
+ 	
+ 	
+
+ 	glColor3f(0.8,0.8,0.8);
+ 	glPushMatrix();
+ 	glTranslatef(lx,ly,lz);
+ 	glScalef(sx,sy,sz);
+ 	glutSolidCube(20.0);
+ 	glPopMatrix();
+ 	
+ 	glPushMatrix();
+ 	glTranslatef(-lx,ly,lz);
+ 	glScalef(sx,sy,sz);
+ 	glutSolidCube(20.0);
+ 	glPopMatrix();
+ 	
+ 	glPushMatrix();
+ 	glTranslatef(lx,ly,-lz);
+ 	glScalef(sx,sy,sz);
+ 	glutSolidCube(20.0);
+ 	glPopMatrix();
+ 	
+ 	glPushMatrix();
+ 	glTranslatef(-lx,ly,-lz);
+ 	glScalef(sx,sy,sz);
+ 	glutSolidCube(20.0);
+ 	glPopMatrix();
+ 	
  	
  	glFlush();
 	glutSwapBuffers();
+}
+
+void mouse(int btn, int state, int x, int y)
+{
+	if(btn==GLUT_LEFT_BUTTON && state == GLUT_DOWN) viewer[0] = 5;
+	if(btn==GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) viewer[1] = 5;
+	if(btn==GLUT_RIGHT_BUTTON && state == GLUT_DOWN) viewer[2] = 5;
+	theta[axis] += 2.0;
+	if( theta[axis] > 360.0 ) theta[axis] -= 360.0;
+	display();
 }
 
 void myReshape(int w, int h)
@@ -42,20 +107,29 @@ void myReshape(int w, int h)
 	glClearColor(0.0,0.5,1.0,1.0);
 	
 	float x=20.0,y=20.0,z=20.0;
+	
  	glViewport(0, 0, w, h);
 
 /* Use a perspective view */
 
  	glMatrixMode(GL_PROJECTION); 
  	glLoadIdentity();
-	if(w<=h) 
+ 	gluPerspective(100.0,(GLfloat) w/ (GLfloat) h,0.6,200.0);
+/*	if(w<=h) 
 		glFrustum(-x, x, -y * (GLfloat) h/ (GLfloat) w, y* (GLfloat) h / (GLfloat) w, 2.0, z);
 	else 
 		glFrustum(-x * (GLfloat) w/ (GLfloat) h, x* (GLfloat) w / (GLfloat) h,-y ,y, 2.0, z);
-
+*/
 
 
  glMatrixMode(GL_MODELVIEW);
+}
+
+void myinit()
+{
+	glClearColor(0.0,0.5,1.0,1.0);
+	glMatrixMode(GL_PROJECTION); 
+	gluOrtho2D(0.0,20.0,0.0,500.0);
 }
 
 void  main(int argc, char **argv)
@@ -65,6 +139,7 @@ void  main(int argc, char **argv)
  glutInitWindowSize(700, 700);
  glutCreateWindow("Petrol Bunk");
  glutReshapeFunc(myReshape);
+ //myinit();
  glutDisplayFunc(display);
 	//glutMouseFunc(mouse);
 	//glutKeyboardFunc(keys);
